892c84fcf4e87107ac2d3fa329736b85bffce463
Dylan Morison <dmorison@oxy.edu>
2020-09-23 02:23:46 -0700

    first github commit

---
+++ config/keys.js
@@ -0,0 +1,2 @@
+// clientID: 234251067173-oasjrlf1rh9aqa03dvc4i0eml8gbtmuj.apps.googleusercontent.com
+// clientSecret: xgxXHaTmHg5FLxsh_DqrAIBq
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ stephen: 'grider' });
-});
+// passport beware of the new strategy: GoogleStrategy
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
