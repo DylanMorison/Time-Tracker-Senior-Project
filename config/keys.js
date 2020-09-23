962775753ecfe8d7b05b4ebe725423686af5e363
Dylan Morison <dmorison@oxy.edu>
2020-09-23 02:25:19 -0700

    first github commit

--- config/keys.js
+++ config/keys.js
@@ -1,2 +1,7 @@
-// clientID: 234251067173-oasjrlf1rh9aqa03dvc4i0eml8gbtmuj.apps.googleusercontent.com
-// clientSecret: xgxXHaTmHg5FLxsh_DqrAIBq
+// clientID: 
+// clientSecret: 
+
+module.exports = {
+    googleClientID = "234251067173-oasjrlf1rh9aqa03dvc4i0eml8gbtmuj.apps.googleusercontent.com",
+    googleClientSecret = "xgxXHaTmHg5FLxsh_DqrAIBq"
+}
