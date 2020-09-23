c59015f922d866dc541323d42c3f772808881c37
Dylan Morison <dmorison@oxy.edu>
2020-09-23 15:17:17 -0700

    github

--- config/keys.js
+++ config/keys.js
@@ -4,4 +4,5 @@
 module.exports = {
     googleClientID = "234251067173-oasjrlf1rh9aqa03dvc4i0eml8gbtmuj.apps.googleusercontent.com",
     googleClientSecret = "xgxXHaTmHg5FLxsh_DqrAIBq"
-}
+};
+
--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-
+const keys = require('./config/keys');
 
 const app = express();
 
 // passport beware of the new strategy: GoogleStrategy
-passport.use(new GoogleStrategy());
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken) => {
+			console.log(accessToken);
+		}
+	)
+);
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
