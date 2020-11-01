128c6b0e621552ba297f5acafa25ea1d550e6084
Dylan Morison <dylan@g.morison.org>
2020-10-31 17:39:59 -0700

    changing set up proxy

--- client/src/components/WebSocketMain.js
+++ client/src/components/WebSocketMain.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
+
+
 const ENDPOINT = "http://127.0.0.1:5001";
 
 function WebSocketMain() {
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,18 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
 	app.use(
-        // in production, when deployed to heroku  
-		['/api', '/auth/google'],
+		// in production, when deployed to heroku
+		["/api", "/auth/google"],
 		createProxyMiddleware({
-			target: 'http://localhost:5000'
+			target: "http://localhost:5000"
+		})
+	);
+
+	app.use(
+		// in production, when deployed to heroku
+		["/WebSocketMain"],
+		createProxyMiddleware({
+			target: "http://localhost:5001"
 		})
 	);
 };
