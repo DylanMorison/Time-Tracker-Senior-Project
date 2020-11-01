8234de79df3eec84be03581a403e0596f78361c5
Dylan Morison <dylan@g.morison.org>
2020-10-31 17:28:37 -0700

    begining implementaion of socket.io on server and client

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import ActivityList from './activities/ActivityList';
+import React from "react";
+import { Link } from "react-router-dom";
+import ActivityList from "./activities/ActivityList";
+import WebSocketMain from "./WebSocketMain";
 
 const Dashboard = () => {
 	return (
 		<div>
 			<ActivityList />
+			<WebSocketMain />
 			<div className="fixed-action-btn">
 				<Link to="/activities/new" className="btn-floating btn-large red">
 					<i className="material-icons">add</i>
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-require('./models/User');
-require('./models/Activity');
-require('./models/ActivityInstance');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const http = require("http");
+const socketio = require("socket.io");
+const { Socket } = require("net");
+
+require("./models/User");
+require("./models/Activity");
+require("./models/ActivityInstance");
+require("./services/passport");
 
 mongoose.Promise = global.Promise;
 
@@ -21,19 +25,31 @@ mongoose.connect(keys.MONGO_URI, {
 
 const app = express();
 
-// each of these app.use calls wire up middleware inside our app.
-// middleware are small functions that can be used to modify incoming requests
-// to our app BEFORE they are sent to the route handlers in routes/authRoutes.js
+const server = http.createServer(app);
 
-// Example: we might want to authenticate every single request that comes to our app
-// so we can wire up our middleware here and do just that.
+const io = socketio(server);
 
-// What if we dont want to run these middlewares every single time?
-// We can set up route handlrs that are not run by them.
+const getApiAndEmit = socket => {
+	const response = new Date();
+	// Emitting a new message. Will be consumed by the client
+	socket.emit("FromAPI", response);
+  };
+
+let interval;
+
+io.on("connection", (socket) => {
+	console.log("New client connected");
+	if (interval) {
+		clearInterval(interval);
+	}
+	interval = setInterval(() => getApiAndEmit(socket), 1000);
+	socket.on("disconnected", () => {
+		console.log("Client disconnected");
+		clearInterval(interval);
+	});
+});
 
 app.use(
-	// wtf is this cookieSession?
-	// we implement cookieSession
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
 		keys: [keys.cookieKey]
@@ -45,22 +61,24 @@ app.use(passport.session());
 
 const jsonParser = bodyParser.json();
 
-require('./routes/authRoutes')(app);
-require('./routes/activityRoutes')(app, jsonParser);
-require('./routes/userRoutes')(app, jsonParser);
+require("./routes/authRoutes")(app);
+require("./routes/activityRoutes")(app, jsonParser);
+require("./routes/userRoutes")(app, jsonParser);
 
 // process.env.NODE_ENV is an envirement variable automatically set by heroku
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
 	// Express will serve up production assets
 	// like our main.js file, or main.css file
-	app.use(express.static('client/build'));
+	app.use(express.static("client/build"));
 
 	// Express will serve up the index.html file if it doesn't recognise the route
-	const path = require('path');
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT);
+server.listen(5001, () => console.log(`Listening on port ${5001}`));
